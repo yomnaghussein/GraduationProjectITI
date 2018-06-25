aef2764852876e7330fa4c9c72110f5e9256536a
AsmaaMarazki <asmaa_mm_kamel@yahoo.com>
2018-06-25 06:30:09 -0700

    profile view design

--- app/src/main/java/com/example/iti/gradproject/screens/editprofilescreen/EditProfileScreen.java
+++ app/src/main/java/com/example/iti/gradproject/screens/editprofilescreen/EditProfileScreen.java
@@ -1,8 +1,14 @@
 package com.example.iti.gradproject.screens.editprofilescreen;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.iti.gradproject.R;
 
@@ -13,11 +19,27 @@ public class EditProfileScreen extends AppCompatActivity implements EditProfileC
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.back_imageView)
+    ImageView backImageView;
+    @BindView(R.id.tv_lname)
+    TextView tvLname;
+    @BindView(R.id.tv_email)
+    TextView tvEmail;
+    @BindView(R.id.tv_mobile)
+    TextView tvMobile;
+    @BindView(R.id.btn_signout)
+    Button btnSignout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile_screen);
         ButterKnife.bind(this);
+        backImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
